c5f2d58e3731df10a8696b92779b9bb503622bf8
Frggle <marc.kaepke@gmail.com>
2015-10-23 12:41:04 +0200

    log implementiert. Noch nicht getestet, da kein Zugriff auf HAW-Netz (Intranet)

--- src/praktikum1/MailFile.java
+++ src/praktikum1/MailFile.java
@@ -1,102 +1,159 @@
 package praktikum1;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
 import java.util.Properties;
 
-
 public class MailFile {
-
+	
 	private Socket so;
 	private BufferedReader inFromServer;  // Rueckmeldung vom Server
 	private DataOutputStream outToServer; // An Server schicken
 	private Properties prop;
 	
-	private static String toMailAdress;
-	private static String filePath;
-
 	private String userBase64;
 	private String passwordBase64;
-
+	
+	private String log = "";
+	
+	private final String BOUNDARY = "98766789";
+	
 	public MailFile() {
-		//
-	}	
+		loadServerData();
+	}
 	
 	private void loadServerData() {
 		prop = new Properties();
 		BufferedInputStream input = null;
 		try {
 			input = new BufferedInputStream(new FileInputStream("src/praktikum1/usrProp"));
-
 			prop.load(input);
-		} catch (IOException e) {
+		} catch(IOException e) {
 			System.err.println("Fehler beim Laden der Props" + e);
 		} finally {
-			if (input != null) {
+			if(input != null) {
 				try {
 					input.close();
-				} catch (IOException e2) {
+				} catch(IOException e2) {
 					e2.printStackTrace();
 				}
 			}
 		}
 	}
 	
-	private void send() throws NumberFormatException, UnknownHostException, IOException {
-		userBase64 = Base64.encodeBytes(prop.getProperty("benutzer").getBytes());
-		passwordBase64 = Base64.encodeBytes(prop.getProperty("passwort").getBytes());
+	private String sendMail(String target, String path) {
+		log = "\nLOG START::\r\n";
+		File file = new File(path);
 		
-		
-		so = new Socket(prop.getProperty("hostname"), Integer.parseInt(prop.getProperty("port")));
-		outToServer = new DataOutputStream(so.getOutputStream());
-		inFromServer = new BufferedReader(new InputStreamReader(so.getInputStream()));
-		
-		outToServer.writeBytes("EHLO " + prop.getProperty("hostname") + "\r\n"); 
-		System.err.println(inFromServer.readLine());
-		outToServer.writeBytes("AUTH LOGIN");
-		do{
-			System.err.println(inFromServer.readLine());
-			}while(inFromServer.ready());
-		outToServer.writeBytes(userBase64);
-		System.err.println(inFromServer.readLine());
-		outToServer.writeBytes(passwordBase64);
-		System.err.println(inFromServer.readLine());
-		outToServer.writeBytes("MAIL From: " + prop.getProperty("mailadresse") + "\r\n");
-		System.err.println(inFromServer.readLine());
-		outToServer.writeBytes("RCPT To: " + toMailAdress + "\r\n");
-		System.err.println(inFromServer.readLine());
-		outToServer.writeBytes("DATA\r\n");
-		System.err.println(inFromServer.readLine());
-		outToServer.writeBytes("Subject: " + prop.getProperty("betreff") + "\r\n");
-		outToServer.writeBytes("MIME-Version: 1.0\r\n");
-		outToServer.writeBytes("ContentType: multipart/mixed; boundary=98766789\r\n");
-		outToServer.writeBytes("\r\n");
-		outToServer.writeBytes("--98766789\r\n");
-		outToServer.writeBytes("Content-Transfer-Encoding: quoted-printable\r\n");
-		outToServer.writeBytes("Content-Type: text/plain\r\n");
-		outToServer.writeBytes("\r\n");
-		outToServer.writeBytes(prop.getProperty("inhalt") + "\r\n");
-		outToServer.writeBytes("--98766789\r\n");
-		// TODO Anhang
-		// TODO Ausgabe als log-file
+		try {
+			userBase64 = Base64.encodeBytes(prop.getProperty("benutzer").getBytes());
+			passwordBase64 = Base64.encodeBytes(prop.getProperty("passwort").getBytes());
+						
+			so = new Socket(prop.getProperty("hostname"), Integer.parseInt(prop.getProperty("port")));
+			outToServer = new DataOutputStream(so.getOutputStream());
+			inFromServer = new BufferedReader(new InputStreamReader(so.getInputStream()));
+			
+			send("EHLO " + prop.getProperty("hostname"));
+			receive();
+			send("AUTH LOGIN");
+			receive();
+			send(userBase64);
+			receive();
+			send(passwordBase64);
+			receive();
+			send("MAIL From: " + prop.getProperty("mailadresse"));
+			receive();
+			send("RCPT To: " + target);
+			receive();
+			send("DATA");
+			receive();
+			send("From: ");
+			receive();
+			send("To: ");
+			send("Subject: " + prop.getProperty("betreff"));
+			send("MIME-Version: 1.0");
+			send("ContentType: multipart/mixed; boundary=" + BOUNDARY);
+			
+			send("--" + BOUNDARY);
+			send("Content-Transfer-Encoding: quoted-printable");
+			send("Content-Type: text/plain");
+			send(prop.getProperty("inhalt"));
+			send("--" + BOUNDARY);
+			
+			send("Content-Transfer-Encoding: base64");
+			send("Content-Type: application/txt");
+			send("Content-Disposition: attachment; filename=" + file.getName());
+			send(Base64.encodeBytes(Files.readAllBytes(file.toPath())));
+			send("--" + BOUNDARY + "--");
+			
+			send(".");
+			send("QUIT");
+			receive();
+			
+		} catch(NumberFormatException e) {
+			System.err.println("Falscher Port");
+		} catch(UnknownHostException e) {
+			System.err.println("Hostname nicht gefunden");
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				inFromServer.close();
+				outToServer.close();
+				so.close();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return log;
 	}
 	
+	/**
+	 * Empfängt die Antworten vom Server
+	 * @throws IOException
+	 */
+	private void receive() throws IOException {
+		do {
+			String s = inFromServer.readLine(); // "Hoere" Server zu
+			System.err.println("<< " + s);		// Ausgabe auf Konsole, was Server antwortet
+			log += "<<" + s + "\r" + "\n";		// Schreibe log
+		} while(inFromServer.ready());
+	}
+	
+	/**
+	 * Sendet die Anfragen zum Server
+	 * @throws IOException
+	 */
+	private void send(String s) throws IOException {
+		System.err.println(">> " + s);			// Ausgabe auf Konsole, was zum Server gesendet wird
+		outToServer.writeBytes(s + "\r" + "\n");// "Spreche" mit Server
+		log += ">>" + s + "\r" + "\n";			// Schreibe log
+	}
+	
+	/**
+	 * 
+	 * @param args[0] = Empfaenger-Mailadresse, args[1] = Dateianhang-Pfad 
+	 */
 	public static void main(String[] args) {
-//		toMailAdress = args[0];
-//		filePath = args[1];
 		MailFile mf = new MailFile();
-		mf.loadServerData();
+		String log = mf.sendMail(args[0], args[1]);
 		try {
-			mf.send();
-		} catch (NumberFormatException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+			File logFile = new File(args[1] + "/log.txt");
+			FileWriter writer = new FileWriter(logFile);
+			writer.write(log);
+			writer.flush();
+			writer.close();
+		} catch(IOException e) {
+			System.err.println("Fehler bei log-Erstellung");
+		} 
 	}
 }
